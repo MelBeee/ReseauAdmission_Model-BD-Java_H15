2acc3aa14c9f5adeee6ed35a33d3f238dfd07f9c
MelBeee <mel.beee@deepteal.com>
2015-04-22 14:59:38 -0400

    Ajout Fichier VS pour Prototype App Local

---
+++ ReseauAdmissionAppLocale/ReseauAdmissionAppLocale/rootForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ReseauAdmissionAppLocale
+{
+   partial class rootForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.panel2 = new System.Windows.Forms.Panel();
+         this.panel3 = new System.Windows.Forms.Panel();
+         this.panel4 = new System.Windows.Forms.Panel();
+         this.panel5 = new System.Windows.Forms.Panel();
+         this.panel6 = new System.Windows.Forms.Panel();
+         this.SuspendLayout();
+         // 
+         // panel2
+         // 
+         this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(221)))), ((int)(((byte)(155)))));
+         this.panel2.Location = new System.Drawing.Point(12, 9);
+         this.panel2.Name = "panel2";
+         this.panel2.Size = new System.Drawing.Size(200, 200);
+         this.panel2.TabIndex = 0;
+         this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+         this.panel2.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+         this.panel2.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+         this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+         // 
+         // panel3
+         // 
+         this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(221)))), ((int)(((byte)(155)))));
+         this.panel3.Location = new System.Drawing.Point(218, 9);
+         this.panel3.Name = "panel3";
+         this.panel3.Size = new System.Drawing.Size(200, 200);
+         this.panel3.TabIndex = 1;
+         this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+         this.panel3.MouseEnter += new System.EventHandler(this.panel2_MouseEnter);
+         this.panel3.MouseLeave += new System.EventHandler(this.panel2_MouseLeave);
+         this.panel3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
+         // 
+         // panel4
+         // 
+         this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(221)))), ((int)(((byte)(155)))));
+         this.panel4.Location = new System.Drawing.Point(424, 9);
+         this.panel4.Name = "panel4";
+         this.panel4.Size = new System.Drawing.Size(200, 200);
+         this.panel4.TabIndex = 1;
+         this.panel4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown);
+         this.panel4.MouseEnter += new System.EventHandler(this.panel3_MouseEnter);
+         this.panel4.MouseLeave += new System.EventHandler(this.panel3_MouseLeave);
+         this.panel4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseUp);
+         // 
+         // panel5
+         // 
+         this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(221)))), ((int)(((byte)(155)))));
+         this.panel5.Location = new System.Drawing.Point(12, 215);
+         this.panel5.Name = "panel5";
+         this.panel5.Size = new System.Drawing.Size(303, 200);
+         this.panel5.TabIndex = 3;
+         this.panel5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseDown);
+         this.panel5.MouseEnter += new System.EventHandler(this.panel4_MouseEnter);
+         this.panel5.MouseLeave += new System.EventHandler(this.panel4_MouseLeave);
+         this.panel5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseUp);
+         // 
+         // panel6
+         // 
+         this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(221)))), ((int)(((byte)(155)))));
+         this.panel6.Location = new System.Drawing.Point(321, 215);
+         this.panel6.Name = "panel6";
+         this.panel6.Size = new System.Drawing.Size(303, 200);
+         this.panel6.TabIndex = 4;
+         this.panel6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel5_MouseDown);
+         this.panel6.MouseEnter += new System.EventHandler(this.panel5_MouseEnter);
+         this.panel6.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+         this.panel6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel5_MouseUp);
+         // 
+         // rootForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(189)))), ((int)(((byte)(124)))));
+         this.ClientSize = new System.Drawing.Size(634, 423);
+         this.Controls.Add(this.panel6);
+         this.Controls.Add(this.panel5);
+         this.Controls.Add(this.panel4);
+         this.Controls.Add(this.panel3);
+         this.Controls.Add(this.panel2);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+         this.Name = "rootForm";
+         this.Text = "ReseauAdmission";
+         this.Load += new System.EventHandler(this.rootForm_Load);
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Panel panel2;
+      private System.Windows.Forms.Panel panel3;
+      private System.Windows.Forms.Panel panel4;
+      private System.Windows.Forms.Panel panel5;
+      private System.Windows.Forms.Panel panel6;
+   }
+}
---
+++ ReseauAdmissionAppLocale/ReseauAdmissionAppLocale/rootForm.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace ReseauAdmissionAppLocale
+{
+   public partial class rootForm : Form
+   {
+      // variable contenant la connection a la bd 
+      OracleConnection oraconnPrincipale = new OracleConnection();
+      // boolean pour stocker si le form est connecté ou non
+      public bool connection = false;
+
+      public rootForm()
+      {
+         InitializeComponent();
+      }
+
+      private void rootForm_Load(object sender, EventArgs e)
+      {
+         if (!connection)
+         {
+            string Dsource = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+                              "(HOST=205.237.244.251)(PORT=1521)))" +
+                              "(CONNECT_DATA=(SERVICE_NAME=ORCL.clg.qc.ca)))";
+            string user = "boucherm";
+            string passwd = "ORACLE2";
+            string chaineconnection = "Data Source = " + Dsource + ";User Id =" + user + "; Password =" + passwd;
+
+            try
+            {
+               oraconnPrincipale.ConnectionString = chaineconnection;
+               oraconnPrincipale.Open();
+               connection = true;
+            }
+            catch (OracleException ex)
+            {
+               SwitchException(ex);
+            }
+         }
+      }
+
+      private void panel1_MouseLeave(object sender, EventArgs e)
+      {
+         panel2.BackColor = Color.FromArgb(97, 221, 155);
+      } 
+
+      private void panel1_MouseDown(object sender, MouseEventArgs e)
+      {
+         panel2.BackColor = Color.FromArgb(46, 154, 96);
+      }
+
+      private void panel1_MouseEnter(object sender, EventArgs e)
+      {
+         panel2.BackColor = Color.FromArgb(136, 243, 186);
+      }
+
+      private void panel2_MouseLeave(object sender, EventArgs e)
+      {
+         panel3.BackColor = Color.FromArgb(97, 221, 155);
+      }
+
+      private void panel2_MouseDown(object sender, MouseEventArgs e)
+      {
+         panel3.BackColor = Color.FromArgb(46, 154, 96);
+      }
+
+      private void panel2_MouseEnter(object sender, EventArgs e)
+      {
+         panel3.BackColor = Color.FromArgb(136, 243, 186);
+      }
+
+      private void panel3_MouseLeave(object sender, EventArgs e)
+      {
+         panel4.BackColor = Color.FromArgb(97, 221, 155);
+      }
+
+      private void panel3_MouseDown(object sender, MouseEventArgs e)
+      {
+         panel4.BackColor = Color.FromArgb(46, 154, 96);
+      } 
+
+      private void panel3_MouseEnter(object sender, EventArgs e)
+      {
+         panel4.BackColor = Color.FromArgb(136, 243, 186);
+      }
+
+      private void panel4_MouseLeave(object sender, EventArgs e)
+      {
+         panel5.BackColor = Color.FromArgb(97, 221, 155);
+      }
+
+      private void panel4_MouseDown(object sender, MouseEventArgs e)
+      {
+         panel5.BackColor = Color.FromArgb(46, 154, 96);
+      }
+
+      private void panel4_MouseEnter(object sender, EventArgs e)
+      {
+         panel5.BackColor = Color.FromArgb(136, 243, 186);
+      }
+
+      private void panel5_MouseLeave(object sender, EventArgs e)
+      {
+         panel6.BackColor = Color.FromArgb(97, 221, 155);
+      }
+
+      private void panel5_MouseDown(object sender, MouseEventArgs e)
+      {
+         panel6.BackColor = Color.FromArgb(46, 154, 96);
+      }
+
+      private void panel5_MouseEnter(object sender, EventArgs e)
+      {
+         panel6.BackColor = Color.FromArgb(136, 243, 186);
+      }
+
+      private void panel4_MouseUp(object sender, MouseEventArgs e)
+      {
+         panel5.BackColor = Color.FromArgb(136, 243, 186);
+      }
+      private void panel1_MouseUp(object sender, MouseEventArgs e)
+      {
+         panel2.BackColor = Color.FromArgb(136, 243, 186);
+      }
+      private void panel2_MouseUp(object sender, MouseEventArgs e)
+      {
+         panel3.BackColor = Color.FromArgb(136, 243, 186);
+      }
+      private void panel3_MouseUp(object sender, MouseEventArgs e)
+      {
+         panel4.BackColor = Color.FromArgb(136, 243, 186);
+      }
+      private void panel5_MouseUp(object sender, MouseEventArgs e)
+      {
+         panel6.BackColor = Color.FromArgb(136, 243, 186);
+      }
+
+      private void SwitchException(OracleException ex)
+      {
+         string DescriptionErreur;
+         // Va chercher l'erreur lancé et set la description dans un string 
+         switch (ex.Number)
+         {
+            case 00001:
+               DescriptionErreur = "Erreur de valeur unique.";
+               break;
+            case 00904:
+               DescriptionErreur = "Nom de colonne invalide ou manquante.";
+               break;
+            case 00933:
+               DescriptionErreur = "Commande SQL invalide.";
+               break;
+            case 00936:
+               DescriptionErreur = "La commande sql exécuté est incorrecte.";
+               break;
+            case 00947:
+               DescriptionErreur = "Il manque des informations dans la commande sql exécuté.";
+               break;
+            case 01008:
+               DescriptionErreur = "Une tentative de liaison de variable a échoué";
+               break;
+            case 01017:
+               DescriptionErreur = "Mot de passe ou nom d'utilisateur invalide. \nConnection non établi.";
+               break;
+            case 01036:
+               DescriptionErreur = "Nom de variable invalide ou manquante";
+               break;
+            case 01400:
+               DescriptionErreur = "Vous ne pouvez pas ajouter une colonne avec une valeur null.";
+               break;
+            case 01407:
+               DescriptionErreur = "Vous ne pouvez pas mettre a jour une colonne avec une valeur null.";
+               break;
+            case 01410:
+               DescriptionErreur = "Vous ne pouvez pas mettre de valeur null.";
+               break;
+            case 01438:
+               DescriptionErreur = "Valeur dépassant la précision maximale dans la BD";
+               break;
+            case 01747:
+               DescriptionErreur = "Tentative d'utilisation d'un mot reservé dans Oracle";
+               break;
+            case 01830:
+               DescriptionErreur = "Le format de date est invalide";
+               break;
+            case 01843:
+               DescriptionErreur = "La date n'est pas valide";
+               break;
+            case 01861:
+               DescriptionErreur = "Le format de date est invalide";
+               break;
+            case 02290:
+               DescriptionErreur = "Tentative d'execution d'une commande qui viole une constrainte Check";
+               break;
+            case 02292:
+               DescriptionErreur = "Tentative de suppression d'une clé lié à une clé étrangère.";
+               break;
+            case 03224:
+               DescriptionErreur = "Pas de connection à Mercure";
+               break;
+            case 12170:
+               DescriptionErreur = "La base de données est indisponible, réessayer plus tard.";
+               break;
+            case 12504:
+               DescriptionErreur = "Connexion impossible. \nLe nom d'instance Oracle est invalide.";
+               break;
+            case 12533:
+               DescriptionErreur = "Connexion impossible. \nLe parametre de connexion d'adresse est invalide.";
+               break;
+            case 12541:
+               DescriptionErreur = "Connexion impossible. \nLa destination est invalide ou pas rejoignable.";
+               break;
+            case 12543:
+               DescriptionErreur = "Connexion impossible. \nVérifiez votre connection internet.";
+               break;
+            default:
+               DescriptionErreur = ex.Message;
+               break;
+         }
+
+         MessageBox.Show(DescriptionErreur, "Erreur #" + ex.Number.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+      }
+   
+   }
+}
