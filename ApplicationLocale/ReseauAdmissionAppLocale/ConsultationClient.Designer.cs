b42637fa8f0fc40a011682c5a133f1300331e119
MelBeee <mel.beee@deepteal.com>
2015-05-01 14:57:22 -0400

    Consultation UserFacture :end:

--- ApplicationLocale/ReseauAdmissionAppLocale/ConsultationClient.Designer.cs
+++ ApplicationLocale/ReseauAdmissionAppLocale/ConsultationClient.Designer.cs
@@ -28,100 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-         this.BTN_Fermer = new System.Windows.Forms.Button();
-         this.label2 = new System.Windows.Forms.Label();
-         this.textBox1 = new System.Windows.Forms.TextBox();
-         this.dataGridView1 = new System.Windows.Forms.DataGridView();
-         this.NumFacture = new System.Windows.Forms.DataGridViewTextBoxColumn();
-         this.Imprimé = new System.Windows.Forms.DataGridViewTextBoxColumn();
-         this.NbreBillet = new System.Windows.Forms.DataGridViewTextBoxColumn();
-         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-         this.SuspendLayout();
-         // 
-         // BTN_Fermer
-         // 
-         this.BTN_Fermer.Font = new System.Drawing.Font("Forte", 13.2F);
-         this.BTN_Fermer.Location = new System.Drawing.Point(90, 252);
-         this.BTN_Fermer.Margin = new System.Windows.Forms.Padding(2);
-         this.BTN_Fermer.Name = "BTN_Fermer";
-         this.BTN_Fermer.Size = new System.Drawing.Size(72, 27);
-         this.BTN_Fermer.TabIndex = 2;
-         this.BTN_Fermer.Text = "Fermer";
-         this.BTN_Fermer.UseVisualStyleBackColor = true;
-         this.BTN_Fermer.Click += new System.EventHandler(this.BTN_Fermer_Click);
-         // 
-         // label2
-         // 
-         this.label2.AutoSize = true;
-         this.label2.Font = new System.Drawing.Font("Forte", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.label2.Location = new System.Drawing.Point(66, 7);
-         this.label2.Name = "label2";
-         this.label2.Size = new System.Drawing.Size(128, 17);
-         this.label2.TabIndex = 7;
-         this.label2.Text = "Numero Adherent";
-         // 
-         // textBox1
-         // 
-         this.textBox1.Location = new System.Drawing.Point(68, 28);
-         this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-         this.textBox1.Name = "textBox1";
-         this.textBox1.Size = new System.Drawing.Size(118, 20);
-         this.textBox1.TabIndex = 8;
-         // 
-         // dataGridView1
-         // 
-         this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-         this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BTN_Fermer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TB_NomID = new System.Windows.Forms.TextBox();
+            this.DGV_facture = new System.Windows.Forms.DataGridView();
+            this.BTN_Afficher = new System.Windows.Forms.Button();
+            this.NumFacture = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Imprime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NbreBillet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_facture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTN_Fermer
+            // 
+            this.BTN_Fermer.Font = new System.Drawing.Font("Forte", 13.2F);
+            this.BTN_Fermer.Location = new System.Drawing.Point(120, 310);
+            this.BTN_Fermer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BTN_Fermer.Name = "BTN_Fermer";
+            this.BTN_Fermer.Size = new System.Drawing.Size(96, 33);
+            this.BTN_Fermer.TabIndex = 2;
+            this.BTN_Fermer.Text = "Fermer";
+            this.BTN_Fermer.UseVisualStyleBackColor = true;
+            this.BTN_Fermer.Click += new System.EventHandler(this.BTN_Fermer_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Forte", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nom ou # d\'adherent";
+            // 
+            // TB_NomID
+            // 
+            this.TB_NomID.Location = new System.Drawing.Point(12, 36);
+            this.TB_NomID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TB_NomID.Name = "TB_NomID";
+            this.TB_NomID.Size = new System.Drawing.Size(191, 22);
+            this.TB_NomID.TabIndex = 8;
+            // 
+            // DGV_facture
+            // 
+            this.DGV_facture.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_facture.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.NumFacture,
-            this.Imprimé,
+            this.Imprime,
             this.NbreBillet});
-         this.dataGridView1.Location = new System.Drawing.Point(9, 53);
-         this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
-         this.dataGridView1.Name = "dataGridView1";
-         this.dataGridView1.RowHeadersVisible = false;
-         this.dataGridView1.RowTemplate.Height = 24;
-         this.dataGridView1.Size = new System.Drawing.Size(235, 194);
-         this.dataGridView1.TabIndex = 9;
-         // 
-         // NumFacture
-         // 
-         this.NumFacture.HeaderText = "Facture";
-         this.NumFacture.Name = "NumFacture";
-         this.NumFacture.ReadOnly = true;
-         this.NumFacture.Width = 80;
-         // 
-         // Imprimé
-         // 
-         this.Imprimé.HeaderText = "Imprimé";
-         this.Imprimé.Name = "Imprimé";
-         this.Imprimé.ReadOnly = true;
-         this.Imprimé.Width = 80;
-         // 
-         // NbreBillet
-         // 
-         this.NbreBillet.HeaderText = "Nombre Billets";
-         this.NbreBillet.Name = "NbreBillet";
-         this.NbreBillet.ReadOnly = true;
-         this.NbreBillet.Width = 70;
-         // 
-         // ConsultationClient
-         // 
-         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(189)))), ((int)(((byte)(124)))));
-         this.ClientSize = new System.Drawing.Size(252, 284);
-         this.Controls.Add(this.dataGridView1);
-         this.Controls.Add(this.textBox1);
-         this.Controls.Add(this.label2);
-         this.Controls.Add(this.BTN_Fermer);
-         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-         this.Margin = new System.Windows.Forms.Padding(2);
-         this.Name = "ConsultationClient";
-         this.Text = "ConsultationClient";
-         this.Load += new System.EventHandler(this.ConsultationClient_Load);
-         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-         this.ResumeLayout(false);
-         this.PerformLayout();
+            this.DGV_facture.Location = new System.Drawing.Point(12, 65);
+            this.DGV_facture.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DGV_facture.Name = "DGV_facture";
+            this.DGV_facture.RowHeadersVisible = false;
+            this.DGV_facture.RowTemplate.Height = 24;
+            this.DGV_facture.Size = new System.Drawing.Size(313, 239);
+            this.DGV_facture.TabIndex = 9;
+            // 
+            // BTN_Afficher
+            // 
+            this.BTN_Afficher.Font = new System.Drawing.Font("Forte", 13.2F);
+            this.BTN_Afficher.Location = new System.Drawing.Point(210, 8);
+            this.BTN_Afficher.Name = "BTN_Afficher";
+            this.BTN_Afficher.Size = new System.Drawing.Size(115, 50);
+            this.BTN_Afficher.TabIndex = 10;
+            this.BTN_Afficher.Text = "Afficher";
+            this.BTN_Afficher.UseVisualStyleBackColor = true;
+            this.BTN_Afficher.Click += new System.EventHandler(this.BTN_Afficher_Click);
+            // 
+            // NumFacture
+            // 
+            this.NumFacture.HeaderText = "Facture";
+            this.NumFacture.Name = "NumFacture";
+            this.NumFacture.ReadOnly = true;
+            this.NumFacture.Width = 80;
+            // 
+            // Imprime
+            // 
+            this.Imprime.HeaderText = "Imprimé";
+            this.Imprime.Name = "Imprime";
+            this.Imprime.ReadOnly = true;
+            this.Imprime.Width = 80;
+            // 
+            // NbreBillet
+            // 
+            this.NbreBillet.HeaderText = "Nombre Billets";
+            this.NbreBillet.Name = "NbreBillet";
+            this.NbreBillet.ReadOnly = true;
+            this.NbreBillet.Width = 70;
+            // 
+            // ConsultationClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(189)))), ((int)(((byte)(124)))));
+            this.ClientSize = new System.Drawing.Size(336, 350);
+            this.Controls.Add(this.BTN_Afficher);
+            this.Controls.Add(this.DGV_facture);
+            this.Controls.Add(this.TB_NomID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BTN_Fermer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ConsultationClient";
+            this.Text = "ConsultationClient";
+            this.Load += new System.EventHandler(this.ConsultationClient_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_facture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -129,10 +143,11 @@
 
         private System.Windows.Forms.Button BTN_Fermer;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TB_NomID;
+        private System.Windows.Forms.DataGridView DGV_facture;
+        private System.Windows.Forms.Button BTN_Afficher;
         private System.Windows.Forms.DataGridViewTextBoxColumn NumFacture;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Imprimé;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Imprime;
         private System.Windows.Forms.DataGridViewTextBoxColumn NbreBillet;
     }
 }
--- ApplicationLocale/ReseauAdmissionAppLocale/ConsultationClient.cs
+++ ApplicationLocale/ReseauAdmissionAppLocale/ConsultationClient.cs
@@ -29,7 +29,84 @@ namespace ReseauAdmissionAppLocale
 
         private void ConsultationClient_Load(object sender, EventArgs e)
         {
+            WidthColonne();
+        }
 
+        private void WidthColonne()
+        {
+            int widthdgv = DGV_facture.Width - 3;
+            Imprime.Width = widthdgv / 3;
+            NumFacture.Width = widthdgv / 3;
+            NbreBillet.Width = widthdgv / 3;
+        }
+
+        private void BTN_Afficher_Click(object sender, EventArgs e)
+        {
+            DGV_facture.Rows.Clear();
+            ChercherInfo();
+        }
+
+        private void ChercherInfo()
+        {
+            bool resultat = false;
+            try
+            {
+                OracleCommand oraFacture = new OracleCommand("AppLocale", oraconnPrincipale);
+                oraFacture.CommandText = "AppLocale.GetLesFacturesDesClients";
+                oraFacture.CommandType = CommandType.StoredProcedure;
+
+                OracleParameter IdClient = new OracleParameter("PIDClient", OracleDbType.Int64);
+                IdClient.Direction = ParameterDirection.Input;
+                IdClient.Value = Convert.ToInt64(TB_NomID.Text);
+
+                OracleParameter Curseur = new OracleParameter("Resultat", OracleDbType.RefCursor);
+                Curseur.Direction = ParameterDirection.Output;
+
+                oraFacture.Parameters.Add(IdClient);
+                oraFacture.Parameters.Add(Curseur);
+
+                OracleDataReader OraRead = oraFacture.ExecuteReader();
+
+                while (OraRead.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)DGV_facture.Rows[0].Clone();
+                    row.Cells[0].Value = OraRead.GetInt64(0).ToString();
+                    row.Cells[1].Value = AnalyseColImprime(OraRead.GetString(1).ToString());
+                    row.Cells[2].Value = OraRead.GetInt64(2).ToString();
+                    DGV_facture.Rows.Add(row);
+                    resultat = true; 
+                }
+
+                oraFacture.Dispose();
+                OraRead.Close();
+            }
+            catch(OracleException ioe)
+            {
+
+            }
+            catch(Exception ioe)
+            {
+
+            }
+            finally
+            {
+                if (!resultat)
+                {
+                    MessageBox.Show("Utilisateur n'existe pas/Aucune facture trouvée");
+                }
+            }
+        }
+
+        private string AnalyseColImprime(string imp)
+        {
+            string returnvalue = "Oui";
+
+            if(imp == "1")
+            {
+                returnvalue = "Non";
+            }
+
+            return returnvalue;
         }
     }
 }
